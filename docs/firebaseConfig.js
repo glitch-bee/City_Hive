f8e5b666f2ba0eb719fb511506c2ccd937371617
focuszero1 <ushergay@gmail.com>
2025-06-04 08:22:41 -0400

    Add firebase configuration file for initializing Firebase services

---
+++ docs/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Copy this file to firebaseConfig.local.js and fill in your Firebase credentials
+const firebaseConfig = {
+  apiKey: "AIzaSyBxuqejpdAdgltobX7tFD_Du6UE9_dTp_c",
+  authDomain: "city-hive-90f1e.firebaseapp.com",
+  projectId: "city-hive-90f1e",
+  storageBucket: "city-hive-90f1e.firebasestorage.app",
+  messagingSenderId: "111416325340",
+  appId: "1:111416325340:web:0804bbbed2647f57ce7019",
+  measurementId: "G-LY3CH0HXDK"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();

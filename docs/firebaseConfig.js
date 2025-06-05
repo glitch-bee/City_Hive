446dc86ae9c0de88568a02aefd9e7c83483504f4
focuszero1 <ushergay@gmail.com>
2025-06-04 23:43:58 -0400

    Fix storageBucket URL in Firebase configuration

--- docs/firebaseConfig.js
+++ docs/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBxuqejpdAdgltobX7tFD_Du6UE9_dTp_c",
   authDomain: "city-hive-90f1e.firebaseapp.com",
   projectId: "city-hive-90f1e",
-  storageBucket: "city-hive-90f1e.firebasestorage.app",
+  storageBucket: "city-hive-90f1e.appspot.com", // <-- FIXED!
   messagingSenderId: "111416325340",
   appId: "1:111416325340:web:0804bbbed2647f57ce7019",
   measurementId: "G-LY3CH0HXDK"
